46a2d3deaa02842a31040b0392f3d5866d4de2bb
bumblebee16 <aishwarya1604@gmail.com>
2016-09-09 17:09:06 +0530

    Updated

--- Adarna/Partner/views.py
+++ Adarna/Partner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def partner(request):
+    template = loader.get_template('Partner/ngo.html')
+    return HttpResponse(template.render(request))
+
+
